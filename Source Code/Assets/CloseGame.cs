b378f6033ea07d165fceac44fa0dc630bb24eaa7
DoyleLab <amohideen99>
2018-10-23 16:42:42 -0700

    Changed endGame to CloseGame

--- Source Code/Assets/CloseGame.cs
+++ Source Code/Assets/CloseGame.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitGame : MonoBehaviour
-{ 
-    public void exit()
+public class CloseGame : MonoBehaviour {
+
+	public void exit()
     {
         Application.Quit();
     }
